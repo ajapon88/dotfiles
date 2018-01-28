509b025fc6645b8e06cbc25807972605e2527cd5
ajapon88 <ajapon88@gmail.com>
2018-01-29 01:20:58 +0900

    add bin dir

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 DOTFILES_PATH=$(cd $(dirname "${0}") ; pwd)
+cd "${DOTFILES_PATH}"
+
 LN_FORCE=
 while getopts f opt
 do
@@ -15,3 +17,10 @@ ln -i -s ${LN_FORCE} "${DOTFILES_PATH}/.bashrc" "${HOME}/.bashrc"
 ln -i -s ${LN_FORCE} "${DOTFILES_PATH}/.bash_profile" "${HOME}/.bash_profile"
 ln -i -s ${LN_FORCE} "${DOTFILES_PATH}/.gitconfig" "${HOME}/.gitconfig"
 
+[ ! -e "${HOME}/bin" ] && mkdir ${HOME}/bin
+
+if [ -d "${DOTFILES_PATH}/bin" ]; then
+  for file in `ls bin`; do
+    ln -i -s ${LN_FORCE} "${DOTFILES_PATH}/bin/${file}" "${HOME}/bin/${file}"
+  done
+fi
