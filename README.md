9ab3fb1134cd0699fcb10e9b59474833af4246f0
ajapon88 <ajapon88@gmail.com>
2024-01-18 18:09:59 +0900

    add install.sh

---
+++ README.md
@@ -0,0 +1,3 @@
+## install
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ajapon88/dotfiles/HEAD/install.sh)"
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -e
+
+git clone https://github.com/ajapon88/dotfiles ~/repos/github.com/ajapon88/dotfiles
+cd ~/repos/github.com/ajapon88/dotfiles
+./setup.sh
